479b008384098fb22d1e5448435e95b453bd5edf
spectrenjuguna <peternjugunaw25@gmail.com>
2026-08-05 10:38:03 +0300

    moring one

--- javascript/datatypes.js
+++ javascript/datatypes.js
@@ -101,3 +101,105 @@ NON PRIMITIVE DATA TYPES {
     console.log(typeof(paymentId));
       console.log(typeof(success));
     
+      //arrays >>this refers to agroup or related sata ,
+     /// arrays are instantiated form index zero 
+/*       arraysare defined in two ways {
+        using square bracket method
+
+        let fruits=['mangoes','bananas','pineapple','strawberry',12,true]
+
+
+        using the array constructor(new Array())
+
+        let fruitTwo=new Array('mangoes','bananas','pineapple','strawberry')
+
+        //multi-dimensinal arrya
+
+        //we nest an array inside an array 
+
+      } */
+             let vehicle=['Subaru',['Hilux','Mark X',"premio",'Vitz'],'Tesla','Honda','Isuzu','Audi']
+
+
+             console.log(vehicle[1][1]);
+             
+
+             //objects>>these are a storage data types that offers key value pair methond when it comes to storing data and it 
+             //allows one to retrive these data via the dot notation for generic use and the square brackets when you want to change the values 
+             //we can have nexted objects and also have object inside arrays  whic helps in looping as a forma of storage 
+             //objects are defined by the curly brackets 
+             //the key -value pairs ae seperated via a full colon 
+
+ //single object with key and value pairs seperated via commas and only the last key value pair does not have a comma 
+ //if you want to return object data chained by string sentences we use backticks as a method of string concatenation and interpolation 
+ //we use the dollar sign to embed the object with in and the curly brackets 
+             let student={
+              name:'Peter Njuguna',
+              course:'ML,DATA ENGINEERING',
+              year:2026,
+              isPaid:true,
+              duration:'12 months'
+             }
+             
+             console.log( `The student named ${student.name} enrolled in a course called ${student.course} 
+              in the year ${student.year} set for a duration 
+              of ${student.duration}
+              `);
+             
+             //array of objects 
+             let eldohubStudent=[
+              {
+              name:'Peter Njuguna',
+              course:'ML,DATA ENGINEERING',
+              year:2026,
+              isPaid:true,
+              duration:'12 months'
+             },
+             {
+              name:'Trizzah',
+              course:'ML,DATA ENGINEERING',
+              year:2026,
+              isPaid:true,
+              duration:'12 months'
+             },
+             {
+              name:'Neema',
+              course:'ML,DATA ENGINEERING',
+              year:2026,
+              isPaid:true,
+              duration:'12 months'
+             },
+             {
+              name:'Velter',
+              course:'ML,DATA ENGINEERING',
+              year:2026,
+              isPaid:true,
+              duration:'12 months'
+             },
+             {
+              name:'Liz',
+              course:'ML,DATA ENGINEERING',
+              year:2026,
+              isPaid:true,
+              duration:'12 months'
+             },
+             {
+              name:'  Wencellious',
+              course:'ML,DATA ENGINEERING',
+              year:2026,
+              isPaid:true,
+              duration:'12 months'
+             },
+             {
+              name:' Radion',
+              course:'ML,DATA ENGINEERING',
+              year:2026,
+              isPaid:true,
+              duration:'12 months'
+             }
+             ]
+
+
+
+               
+             //functions>>
