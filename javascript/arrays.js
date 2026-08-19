780f90526431ef14473ba3c9548244f1bb70461b
spectrenjuguna <peternjugunaw25@gmail.com>
2026-08-19 11:48:59 +0300

    map

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -36,3 +36,34 @@ console.log(arraySentence);
 
 
 //iteration methods
+//forEach()>>this is a high order function that is used to loop through an array iterating through every element 
+let newForEach=fruits.forEach(function(fruit){
+    console.log(fruit);
+    //any other logical operations
+    
+}
+
+)
+//map method>>it return a new array from an existing array by altering the array to give a new one //looping trhough every iiteration
+
+let numbers=[1,4,5,2000,2345454,645,232,2434345]
+let value=numbers.map(number =>{
+     return  number*2
+})
+console.log(value);
+console.log(numbers);
+
+
+let persons=[
+    {name:'Peter',score:99},
+    {name:'Velter',score:120},
+    {name:'Wencellious',score:110},
+    {name:'Trizzah',score:112},
+    {name:'Liz',score:1000},
+]
+let names=persons.map((person)=>{
+   let persona=person.name
+   let newOne=persona
+   return newOne 
+})
+console.log(names);
